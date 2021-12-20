9726097c0fe171d20df889ad6642a8d28f5c5dd1
guerramorante <mmorante@myuax.com>
2021-12-20 16:22:43 +0100

    Creación de código y de variables

---
+++ Estrella_Regular.py
@@ -0,0 +1,10 @@
+def dibujar_estrella(dimensión,color):
+    contar = 0
+    ángulo = 144
+
+    while contar <= 9:
+        turtle.forward(dimensión)
+        turtle.right(ángulo)
+        contar += 1
+    return
+dibujar_estrella(180, "red")    
